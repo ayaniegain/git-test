e765a3362c40bdb3ed04f30b51099dd1efd9e651
ayaniegain <ayaniegain@gmail.com>
2024-05-30 13:31:00 +0530

    text changes second  from mohit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ function App() {
     <h1 className="text-3xl font-bold underline">
     Hello mohit!
   </h1>
+    <h1 className="text-3xl font-bold underline">
+   get a job. 
+  </h1>
     </>
   )
 }
